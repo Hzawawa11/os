60c07b48ad801eb0464b451d292d63dc90c4636b
Hzawawa11 <hzawawa11@gmail.com>
2017-01-10 23:05:48 +0900

    isuses pipe 8[Kbytes]

--- exercise_s2-2.c
+++ exercise_s2-2.c
@@ -2,42 +2,51 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define SIZ BUFSIZ+200
 
 int main(int argc, char** argv){
-  int pid, p_fd[2];
+  int pid, p_fd[2], fd, num;
   char *new_program, **new_argv;
+
+  char buf[BUFSIZ] = {'\0'};
 
   if(argc < 3){
     fprintf(stderr, "usage: %s string command arg...\n", argv[0]);
     exit(1);
   }
 
-  int fd, num;
-  char buf[BUFSIZ] = {'\0'};
   if((fd = open(argv[1], O_RDWR|O_CREAT|O_TRUNC, 0666)) < 0){
     perror(argv[1]);
     exit(1);
   }
 
+  printf("SIZE: [%d]\n", SIZ);
+
   pipe(p_fd);
 
-  // while(1){
-    // if((num =read(0, buf, BUFSIZ)) == 0) break;
-  num = read(0, buf, BUFSIZ);
-  printf("Input : [%s]\n", buf);
-  write(p_fd[1], buf, num);
-  write(fd, buf, num);
+  // p_fd[0]; output
+  // p_fd[1]; input
+  // input; 0
+  // output; 1
+  // num = read(0, buf, SIZ);
+  int cnt = 0;
+  while((num = read(0, buf, 1))&& cnt < BUFSIZ){
+    cnt += 1;
+    write(p_fd[1], buf, num);
+  }
 
   if((pid = fork()) == 0){
+
     close(p_fd[1]);
     close(0);
     dup(p_fd[0]);
     close(p_fd[0]);
 
-    new_program = argv[2];
-    new_argv = &argv[2];
-    execvp(new_program, new_argv);
-    perror(new_program);
+    while((num = read(0, buf, 1)))
+      write(fd, buf, num);
+
     exit(1);
   }
 
@@ -46,11 +55,17 @@ int main(int argc, char** argv){
     exit(1);
   }
 
+  // wait(NULL);
+  close(0);
+  dup(p_fd[0]);
   close(p_fd[0]);
-    // close(0);
-    // dup(p_fd[0]);
-    // close(p_fd[0]);
-  // }
+
+  // new_program = argv[2];
+  // new_argv = &argv[2];
+
+  // execvp(new_program, new_argv);
+
+  // printf("INPUT[%s]\n", buf);
 
   exit(1);
 }
